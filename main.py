e92d6d6448ed22496e6d7a16c7ccc5320c439835
rocketorbit <33794532+rocketorbit@users.noreply.github.com>
2023-11-19 21:24:49 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#if the image is bigger than 64x64, there are chance that the code will be too long and it does not work.
+
+scale = 10 #adjust the number to make the image bigger. 1 is normal size.
+
+from PIL import Image
+from pyperclip import copy
+
+image = Image.open("image.png") #the name of the image file.
+pixels = list(image.getdata())
+
+width = image.size[0]
+
+x = 0
+y = 0
+ret = 'print("'
+
+for r, g, b in pixels:
+    ret += f'<size={2 * scale}><pos={x * scale}><voffset=-{y * scale}><sprite=0 color=#{r:02x}{g:02x}{b:02x}>'
+    x += 1
+    if x == width:
+        y += 1
+        x = 0
+
+ret += '</voffset></pos></size>")'
+
+print(ret)
+copy(ret)
